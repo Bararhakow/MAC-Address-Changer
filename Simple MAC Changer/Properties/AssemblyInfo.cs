76cffa9d69721706b04f5b546fd4c3605b17deff
Denis T. <deadspell21391@gmail.com>
2025-10-20 11:46:15 +0200

    Upload Files

---
+++ Simple MAC Changer/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Simple MAC Changer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Simple MAC Changer")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("961D5E5F-F8D8-4B4F-849E-EA201A206ECF")]
+[assembly: AssemblyVersion("5.65.38.6036")]
+[assembly: AssemblyFileVersion("5.65.38.6036")]
